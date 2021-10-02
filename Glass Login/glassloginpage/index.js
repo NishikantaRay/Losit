aac0fa937ac13978bbc3b811d850d64bb5688159
SoubhagyaSonu <88227246+SoubhagyaSonu@users.noreply.github.com>
2021-10-02 15:16:27 +0530

    Add files via upload

---
+++ Glass Login/glassloginpage/index.js
@@ -0,0 +1,11 @@
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
