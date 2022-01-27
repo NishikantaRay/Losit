eaf56fd6764606d1607d11b0e8aebb3ca1a0a03a
Nishikanta Ray <nishikantaray1@gmail.com>
2022-01-27 23:01:36 +0530

    added navbar and scrolleffect

--- Main-website/v1/src/app/pages/home/home.component.ts
+++ Main-website/v1/src/app/pages/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const myTest: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
+    myTest();
   }
 
 }
---
+++ Main-website/v1/src/assets/app.js
@@ -0,0 +1,10 @@
+function myTest() {
+    var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+      nav.classList.add('bg-dark', 'shadow');
+    } else {
+      nav.classList.remove('bg-dark', 'shadow');
+    }
+  });
+}
