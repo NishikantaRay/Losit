3bafd6fdde14f25d032bca4ea0b9148531a4cc73
shubham <shubham22121@gmail.com>
2022-10-15 00:20:26 +0530

    [CHORE] controllers directory added

---
+++ backend/controllers/card.controller.js
@@ -0,0 +1,47 @@
+const Card = require("../model/card.model");
+
+// Create and Save a new Card
+exports.create = (req, res) => {
+  const cardData = {
+    name: req.body.name,
+    description: req.body.description,
+    status: req.body.status,
+    priority: req.body.priority,
+    imgUrl: req.body.imgUrl,
+    socialLinks: req.body.socialLinks,
+  };
+  const new_card = new Card(cardData);
+  new_card.save(function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(result);
+      res.status(201).send(result);
+    }
+  });
+};
+
+// Get all Card
+exports.findAll = (req, res) => {
+  Card.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log(docs);
+      res.send(docs);
+    }
+  });
+};
+
+// Delete A Card
+exports.delete = async (req, res) => {
+  try {
+    await Card.deleteOne({ _id: req.params.id });
+    res.redirect("/card");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/card");
+  }
+};
--- backend/route/card.route.js
+++ backend/route/card.route.js
@@ -1,61 +1,18 @@
 // Importing express module
 const express=require("express")
-const Card = require("../model/card.model")
 
 
 const router=express.Router()
+
+
+const cardController=require("../controllers/card.controller")
+
+
+router.post("/create",cardController.create)
+
+router.get("/findAll",cardController.findAll)
+
+router.delete("/delete/:id",cardController.delete)
+
   
-// Handling request using router
-// Create a Card
-router.post("/add", (req, res, next) => {
-    const cardData = {
-        name : req.body.name,
-        description : req.body.description,
-        status : req.body.status,
-        priority : req.body.priority,
-        imgUrl : req.body.imgUrl,
-        socialLinks : req.body.socialLinks,
-    }
-    const new_card = new Card(cardData)
-    new_card.save(function(err,result){
-        if (err){
-            console.log(err);
-            res.send(err)
-        }
-        else{
-            console.log(result)
-            res.status(201).send(result)
-        }
-    })
-})
-// Get all Card
-router.get("/", (req, res, next) => {
-    Card.find({}, (err, docs) => {
-        if (err) {
-            console.log(err);
-            res.send(err)
-        } else {
-            console.log(docs)
-            res.send(docs)
-        }
-    })
-})
-
-
-        }
-    });
-})
-
-// Delete A Card
-router.get("/delete/:id", async (req, res, next) => {
-    try {
-        await Card.deleteOne({ _id: req.params.id });
-        res.redirect("/card")
-    } catch (err) {
-        console.log(err)
-        res.redirect("/card")
-    }
-})
-
-// Importing the router
 module.exports=router
